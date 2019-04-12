cb7d8a5711c07f1cb25a0fda68fe5b7055a2efc8
zhfeing <zhf666968@gmail.com>
2019-04-12 20:50:09 +0800

    add save entire model

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@
 if [[ $HOSTNAME == "vipa210" ]]; then
     echo "run on vipa210"
     python run.py \
-    --gpu="2" --lr=0.01 --epochs=200 \
+    --gpu="2" --lr=0.01 --epochs=2 \
     --train_v="test" --load_v="test" \
     --regularize=0 --retrain=1 --batch_size=256
 
 elif [[ $HOSTNAME == "vipa-Precision-Tower-7910" ]]; then
     echo "run on vipa-Precision-Tower-7910"
     python run.py \
-    --gpu="0" --lr=0.01 --epochs=50 \
+    --gpu="1" --lr=0.01 --epochs=40 \
     --train_v="googlenet-2.0" --load_v="googlenet-2.0" \
-    --regularize=1 --retrain=1 --batch_size=256
+    --regularize=0 --retrain=1 --batch_size=256
 else
     echo "unknown host name"
 fi
